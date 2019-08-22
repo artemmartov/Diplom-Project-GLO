a019a19a8f8764284d42d5fa06996c07dc05dd64
Artem <geroyan.artem@mail.ru>
2019-08-22 23:49:43 +0400

    Исправил переменную

--- src/script.js
+++ src/script.js
@@ -1,8 +1,8 @@
 var money = 22;
 var income = 10;
 var addExpenses = 33;
-var deposit = 65;
+var deposit = 70;
 var mission = 93;
-var period = 13;
+var period = 55;
 alert("Привет!");
 console.log("Еще раз привет!!!");
