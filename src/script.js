8e53f60733916e74f5684be318bb539ce521615f
Artem <geroyan.artem@mail.ru>
2019-08-22 21:38:20 +0400

    'восклицания'

--- src/script.js
+++ src/script.js
@@ -1,8 +1,8 @@
 var money = 22;
 var income = 10;
-var addExpenses = 5;
+var addExpenses = 33;
 var deposit = 65;
-var mission = 90;
+var mission = 93;
 var period = 13;
 alert("Привет!");
-console.log("Еще раз привет!");
+console.log("Еще раз привет!!!");
