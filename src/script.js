e4d7f83b19fbf2eb1a55175ea8339f5fac6c2573
Artem <geroyan.artem@mail.ru>
2019-08-22 21:53:26 +0400

    Изменение чисел

--- src/script.js
+++ src/script.js
@@ -1,8 +1,8 @@
-var money = 22;
-var income = 10;
-var addExpenses = 33;
-var deposit = 65;
-var mission = 93;
-var period = 13;
+var money = 220;
+var income = 100;
+var addExpenses = 330;
+var deposit = 650;
+var mission = 930;
+var period = 130;
 alert("Привет!");
 console.log("Еще раз привет!!!");
