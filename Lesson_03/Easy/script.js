b9762f05799cede7c6cd10a16e972225efd3e5c3
Artem <geroyan.artem@mail.ru>
2019-08-26 20:32:15 +0400

    Fixed: Сделал красивее | Lesson03

--- Lesson_03/Easy/script.js
+++ Lesson_03/Easy/script.js
@@ -1,35 +1,34 @@
 'use strict';
 
-let money = +prompt('Ваш месячный доход?');
+let money = +prompt('Ваш месячный доход?'),
 
-let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
 console.log(addExpenses.split(','));
 
-let deposit = confirm('Есть ли у вас депозит в банке?');
+deposit = confirm('Есть ли у вас депозит в банке?'),
 console.log(deposit);
 
-let income = 'фриланс 20000';
+income = 'фриланс 20000',
 
 console.log(typeof money);
 console.log(typeof income);
 console.log(typeof deposit);
 
 
-let oblExpenses1 = prompt('Какие обязательные ежемесячные расходы у вас есть?');
-let costOfExpenses1 = +prompt('Во сколько это обойдется?');
-let oblExpenses2 = prompt('Какие обязательные ежемесячные расходы у вас есть?');
-let costOfExpenses2 = +prompt('Во сколько это обойдется?');
+oblExpenses1 = prompt('Какие обязательные ежемесячные расходы у вас есть?'),
+costOfExpenses1 = +prompt('Во сколько это обойдется?'),
+oblExpenses2 = prompt('Какие обязательные ежемесячные расходы у вас есть?'),
+costOfExpenses2 = +prompt('Во сколько это обойдется?'),
 
-let budgetMonth = money-(costOfExpenses1+costOfExpenses2);
+budgetMonth = money-(costOfExpenses1+costOfExpenses2),
 console.log(budgetMonth);
 
-let mission = 500000; 
+mission = 500000, 
 console.log(Math.ceil(mission/budgetMonth));
 
-let budgetDay = (budgetMonth/30);
+budgetDay = (budgetMonth/30),
 console.log(Math.floor(budgetDay));
 
-let period = 6;
 
 if (budgetDay >= 800){
     console.log('высокий уровень дохода');
