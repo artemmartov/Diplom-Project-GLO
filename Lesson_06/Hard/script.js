96d268f8daab5bb833b89a00a4d190a13f9b2f9f
Artem <geroyan.artem@mail.ru>
2019-08-30 01:37:42 +0400

    Feat: Доп дз | Lesson06

---
+++ Lesson_06/Hard/script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+let arr = ['Понедельник', 'Вторник', 'Среда', 'Четверг'.bold(), 'Пятница', 'Суббота'.italics(), 'Воскресенье'.italics()];
+
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
