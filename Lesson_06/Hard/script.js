505136a19ffc18af15b4f309b491ae436020d8b7
Artem <geroyan.artem@mail.ru>
2019-08-31 21:57:02 +0400

    Feat: Доп задание | Lesson06

--- Lesson_06/Hard/script.js
+++ Lesson_06/Hard/script.js
@@ -1,18 +1,31 @@
 'use strict';
 
-let arr = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+let arr = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
     
 
 for (let i = 0; i < arr.length; i++) {
     console.log(arr[i]);
 }
 
-let elem = document.createElement('p'),
-    week = document.createTextNode(arr),
-    days = document.getElementById('days');
-
-elem.appendChild(week);
-days.parentNode.appendChild(elem);
+let elem = document.createElement('ul'),
+    date = new Date(),
+    days = document.querySelector('.days');
+   
 
 
-console.log(elem);
+
+arr.forEach(function(day, i){
+    let li = document.createElement('li');
+    li.textContent = day;
+    if (day === 'Суббота' || day === 'Воскресенье') {
+        li.classList.add('weekends');
+    }
+    if (i === date.getDay()) {
+        console.log(date.getDay());
+        li.classList.add('today');
+    }
+    elem.appendChild(li);
+});
+
+days.appendChild(elem);
+
