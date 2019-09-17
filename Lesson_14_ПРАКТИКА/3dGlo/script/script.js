7de3059c9e07a95a22b745bbdaa6af9050cf50e4
Artem <geroyan.artem@mail.ru>
2019-09-18 00:19:00 +0400

    Fixed & Feat | исправил и сделал новое дз | Lesson 14

--- Lesson_14_ПРАКТИКА/3dGlo/script/script.js
+++ Lesson_14_ПРАКТИКА/3dGlo/script/script.js
@@ -65,6 +65,8 @@ window.addEventListener('DOMContentLoaded', () => {
                 handlerMenu();
             }
         });
+        
+        
 
     };
 
@@ -152,6 +154,141 @@ window.addEventListener('DOMContentLoaded', () => {
 
     };
     tabs();
+
+    // слайдер
+
+    const slider = () => {
+        const slide = document.querySelectorAll('.portfolio-item'),
+            btn = document.querySelectorAll('.portfolio-btn'),
+            slider = document.querySelector('.portfolio-content'),
+            portfolioDots = document.querySelector('.portfolio-dots');
+
+        let currentSlide = 0,
+            interval;
+        
+
+        const addDots = () => {
+            for(let i = 0; i <= slide.length - 1; i++) {
+                let dots = document.createElement('li');
+                dots.classList.add('dot');
+                if (i === 0) {
+                        dots.classList.add('dot-active');
+            }
+                portfolioDots.appendChild(dots);
+            }
+            
+        };
+        addDots();
+        const dot = document.querySelectorAll('.dot');
+
+        const prevSlide = (elem, index, strClass) => {
+            elem[index].classList.remove(strClass);
+            
+        };
+
+        const nextSlide = (elem, index, strClass) => {
+            elem[index].classList.add(strClass);
+        };
+
+
+        const autoPlaySlide = () => {
+            console.log(dot);
+            prevSlide(slide, currentSlide, 'portfolio-item-active');
+            prevSlide(dot, currentSlide, 'dot-active');
+            currentSlide++;
+            if (currentSlide >= slide.length){
+                currentSlide = 0;
+            }
+            nextSlide(slide, currentSlide, 'portfolio-item-active');
+            nextSlide(dot, currentSlide, 'dot-active');
+
+
+        };
+
+        const startSlide = (time=3000) => {
+            interval = setInterval(autoPlaySlide, time);
+
+        };
+
+        const stopSlide = () => {
+            clearInterval(interval);
+
+        };
+
+        slider.addEventListener('click', (event) => {
+            event.preventDefault();
+            let target = event.target;
+
+            if (!target.matches('.portfolio-btn', 'dot')){
+                return;
+            }
+            prevSlide(slide, currentSlide, 'portfolio-item-active');
+            prevSlide(dot, currentSlide, 'dot-active');
+
+
+            if (target.matches('#arrow-right')){
+                currentSlide++;
+
+            } else if (target.matches('#arrow-left')){
+                currentSlide--;
+
+            } else if (target.matches('dot')){
+                dot.forEach((elem, index) => {
+                    if (elem === target){
+                        currentSlide = index;
+                    }
+
+                });
+            }
+            
+            if (currentSlide >= slide.length){
+                currentSlide = 0;
+            } 
+            if (currentSlide < 0){
+                currentSlide = slide.length - 1;
+            }
+            nextSlide(slide, currentSlide, 'portfolio-item-active');
+            nextSlide(dot, currentSlide, 'dot-active');
+
+
+        });
+        slider.addEventListener('mouseover', (event) =>{
+            if (event.target.matches('.portfolio-btn') || 
+            event.target.matches('.dot')){
+                stopSlide();
+            }
+
+        });
+
+        slider.addEventListener('mouseout', (event) =>{
+            if (event.target.matches('.portfolio-btn') || 
+            event.target.matches('.dot')){
+                startSlide();
+            }
+            
+
+        });
+
+        
+        
+
+
+
+        startSlide(1500);
+        
+        
+
+    };
+    slider();
 });
     
 
+
+// for(let i = 0; i < slide.length; i++) {
+//     let dot = document.createElement('li');
+//     dot.classList.add('dot');
+//     if (currentSlide === i) {
+//             dot.classList.add('dot-active');
+// }
+//     dots.appendChild(dot);
+// }
